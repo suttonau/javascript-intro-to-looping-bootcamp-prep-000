93447651f2dc8a45bd73ce6103cafafe9cf2748c
Sean Sutton Jr <sutton_mjbc@icloud.com>
2018-08-01 15:51:07 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(aray) {
   for(var i = 0; i < 25; i++) {
-    if(i === 0) {
+    if(i===0) {
       console.log("I am ${i} strange loop.")
     }else{
       console.log("I am ${i} strange loops.")
